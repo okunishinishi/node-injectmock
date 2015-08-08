22f20af22404c0996c03b52965c7da9726751067
Taka Okunishi <okunishinishi@gmail.com>
2015-08-08 17:21:45 +0900

    Version incremented to 1.1.2

--- ci/release.js
+++ ci/release.js
@@ -6,13 +6,10 @@
 
 "use strict";
 
-var path = require('path'),
-    apeTasking = require('ape-tasking'),
+process.chdir(__dirname + '/..');
+
+var apeTasking = require('ape-tasking'),
     apeReleasing = require('ape-releasing');
-
-var basedir = path.resolve(__dirname, '..');
-process.chdir(basedir);
-
 
 apeTasking.runTasks('release', [
     function (callback) {
